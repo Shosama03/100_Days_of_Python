7e43d0e025eb0d6b5e840d6d0afc5d0c65326ccf
Shosama69 <rajatsingh20060@gmail.com>
2025-08-14 21:56:00 +0530

    Day_09 Secret-Auction

---
+++ Day_09/README.md
@@ -0,0 +1,18 @@
+## **Day 9 – 100 Days of Python**  
+
+📌 **Today's Focus:**  
+- Understanding dictionaries in Python  
+- Learning how to store and access key-value pairs  
+- Nesting lists and dictionaries for complex data structures  
+
+💡 **Key Learnings:**  
+- Creating and modifying dictionaries  
+- Looping through dictionary keys and values  
+- Nesting data structures for better organization  
+- Using dictionaries to model real-world scenarios  
+
+🛠 **Project of the Day – Secret Auction Program:**  
+Built a program that collects bids from multiple users, stores them in a dictionary, and determines the highest bidder without revealing others’ bids.  
+
+✅ **Progress:**  
+Completed **Day 9** of the challenge! Learned how to use dictionaries effectively to manage data and solve practical problems. 🚀  
---
+++ Day_09/Secret_Auction.py
@@ -0,0 +1,36 @@
+
+from logo import logo
+print(logo)
+from os import system
+print("Welcome to the Secret Auction!")
+
+
+
+def find_highest_bidder(bidding_dictionary):
+    highest_bid=0
+    for bidder in bidding_dictionary:
+        bid_amount=bidding_dictionary[bidder] 
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+bids={}
+
+continue_bidding = True
+while continue_bidding:
+    name = input("What is your name?: ")
+    bid = float(input("What is your bid?: $"))
+    bids[name] = bid
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'. \n ").lower()
+    if should_continue == "no":
+        continue_bidding=False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        system('clear')
+
+        
+        
+
+
